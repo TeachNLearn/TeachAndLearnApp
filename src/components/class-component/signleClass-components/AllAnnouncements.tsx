8c2e7e75414c07fd970e934fe693934363fe4544
Garvit1809 <garvitvarshne18@gmail.com>
2023-08-27 20:48:11 +0530

    setting up single classroom

---
+++ src/components/class-component/signleClass-components/AllAnnouncements.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const AllAnnouncements = () => {
+  return (
+    <Text>AllAnnouncements</Text>
+  )
+}
+
+export default AllAnnouncements
---
+++ src/components/class-component/signleClass-components/Overview.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Overview = () => {
+  return (
+    <Text>Overview</Text>
+  )
+}
+
+export default Overview
